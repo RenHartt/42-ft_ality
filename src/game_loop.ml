c4cca84b2fa4cdf70e0b86da17d0ba39dbddee03
unknown <baptiste.goron@gmail.com>
2025-10-21 05:09:21 +0200

    add image on screen on combo todo: clean screen between combos format output continue crying un ocaml

--- src/game_loop.ml
+++ src/game_loop.ml
@@ -2,49 +2,63 @@
 
 let init_window () =
   match Tsdl.Sdl.init Tsdl.Sdl.Init.video with
-  | Error (`Msg e) -> prerr_endline ("SDL init error: " ^ e); exit 1
+  | Error (`Msg e) ->
+      prerr_endline ("SDL init error: " ^ e); exit 1
   | Ok () ->
-      match Tsdl.Sdl.create_window ~w:640 ~h:360 "ft_ality (inputs test)"
+      match Tsdl.Sdl.create_window
+              ~w:640 ~h:360 "ft_ality (inputs test)"
               Tsdl.Sdl.Window.windowed with
-      | Error (`Msg e) -> prerr_endline ("SDL window error: " ^ e); Tsdl.Sdl.quit (); exit 1
+      | Error (`Msg e) ->
+          prerr_endline ("SDL window error: " ^ e);
+          Tsdl.Sdl.quit (); exit 1
       | Ok w -> w
 
 let pressed sc =
   let ks = Tsdl.Sdl.get_keyboard_state () in
   Bigarray.Array1.get ks sc <> 0
 
-let try_step (a:Automaton.t) (st:Automaton.state) (tok:string) : Automaton.state =
-  match Automaton.step a st tok with
-  | Some s -> s
-  | None ->
-      match Automaton.step a a.Automaton.start tok with
-      | Some s -> s
-      | None   -> st
+let show_bmp (w:Tsdl.Sdl.window) (path:string) : unit =
+  match Tsdl.Sdl.load_bmp path with
+  | Error (`Msg e) ->
+      prerr_endline ("BMP load error: " ^ e)
+  | Ok img ->
+      (match Tsdl.Sdl.get_window_surface w with
+       | Error (`Msg e) ->
+           prerr_endline ("Window surface error: " ^ e)
+       | Ok win_surf ->
+           begin match Tsdl.Sdl.blit_surface img None win_surf None with
+           | Error (`Msg e) -> prerr_endline ("Blit error: " ^ e)
+           | Ok () ->
+               ignore (Tsdl.Sdl.update_window_surface w)
+           end);
+      Tsdl.Sdl.free_surface img
 
-let handle_key (a:Automaton.t) (map:Mapping.t) (st:Automaton.state) (sc:int)
+
+let handle_key (w:Tsdl.Sdl.window) (a:Automaton.t) (map:Mapping.t) (st:Automaton.state) (sc:int)
   : Automaton.state =
   match Mapping.find map sc with
-  | None -> a.Automaton.start
+  | None -> st
   | Some t ->
       Printf.printf "Pressed: %s (%s)\n%!" t.Grammar.label t.token;
       let next =
         match Automaton.step a st t.token with
-        | Some s -> s
-        | None ->
-            match Automaton.step a a.Automaton.start t.token with
-            | Some s -> s
-            | None   -> a.Automaton.start
+        | Some s -> Some s
+        | None -> Automaton.step a a.Automaton.start t.token
       in
-      let finals = Automaton.finals_of a next in
-      if finals <> [] then (
-        List.iter (fun n -> Printf.printf "MOVE: %s\n%!" n) finals;
-        a.Automaton.start
-      ) else
-        next
+      match next with
+      | None -> a.Automaton.start
+      | Some s ->
+          let finals = Automaton.finals_of a s in
+          (match finals with
+           | [] -> s
+           | names ->
+               List.iter (fun n -> Printf.printf "MOVE: %s\n%!" n) names;
+               show_bmp w "data/fatality.bmp";
+               a.Automaton.start)
 
-let rec loop (_w:Tsdl.Sdl.window) (a:Automaton.t) (map:Mapping.t) (st:Automaton.state) (ev:Tsdl.Sdl.event) =
+let rec loop (w:Tsdl.Sdl.window) (a:Automaton.t) (map:Mapping.t) (st:Automaton.state) (ev:Tsdl.Sdl.event) =
   match Tsdl.Sdl.wait_event (Some ev) with
-  | Error (`Msg _) -> loop _w a map st ev
+  | Error (`Msg _) -> loop w a map st ev
   | Ok () ->
       let typ = Tsdl.Sdl.Event.(enum (get ev typ)) in
       match typ with
@@ -53,15 +67,13 @@ let rec loop (_w:Tsdl.Sdl.window) (a:Automaton.t) (map:Mapping.t) (st:Automaton.
           if pressed Tsdl.Sdl.Scancode.escape then ()
           else
             let sc = Tsdl.Sdl.Event.(get ev keyboard_scancode) in
-            let st' = handle_key a map st sc in
-            loop _w a map st' ev
-      | _ -> loop _w a map st ev
+            let st' = handle_key w a map st sc in
+            loop w a map st' ev
+      | _ -> loop w a map st ev
 
 let run (a:Automaton.t) (map:Mapping.t) : unit =
   let w = init_window () in
   Mapping.print map;
-  print_endline "------";
-  print_endline "Press mapped keys (ESC to quit, or close window).";
   let ev = Tsdl.Sdl.Event.create () in
   loop w a map a.Automaton.start ev;
   Tsdl.Sdl.destroy_window w;
--- src/mapping.ml
+++ src/mapping.ml
@@ -8,14 +8,21 @@ let keys : key list =
   [
     Tsdl.Sdl.Scancode.left; Tsdl.Sdl.Scancode.right;
     Tsdl.Sdl.Scancode.up;   Tsdl.Sdl.Scancode.down;
-    Tsdl.Sdl.Scancode.a; Tsdl.Sdl.Scancode.z; Tsdl.Sdl.Scancode.e;
-    Tsdl.Sdl.Scancode.r; Tsdl.Sdl.Scancode.t;
-    Tsdl.Sdl.Scancode.q; Tsdl.Sdl.Scancode.s; Tsdl.Sdl.Scancode.d;
-    Tsdl.Sdl.Scancode.f; Tsdl.Sdl.Scancode.g;
-    Tsdl.Sdl.Scancode.w; Tsdl.Sdl.Scancode.x; Tsdl.Sdl.Scancode.c;
-    Tsdl.Sdl.Scancode.v; Tsdl.Sdl.Scancode.b;
-    Tsdl.Sdl.Scancode.k1; Tsdl.Sdl.Scancode.k2; Tsdl.Sdl.Scancode.k3;
-    Tsdl.Sdl.Scancode.k4; Tsdl.Sdl.Scancode.k5;
+    Tsdl.Sdl.Scancode.q;    Tsdl.Sdl.Scancode.w;
+    Tsdl.Sdl.Scancode.e;    Tsdl.Sdl.Scancode.r;
+    Tsdl.Sdl.Scancode.a;    Tsdl.Sdl.Scancode.s;
+    Tsdl.Sdl.Scancode.z;    Tsdl.Sdl.Scancode.x;
+    Tsdl.Sdl.Scancode.e;    Tsdl.Sdl.Scancode.r;
+    Tsdl.Sdl.Scancode.d;    Tsdl.Sdl.Scancode.f;
+    Tsdl.Sdl.Scancode.c;    Tsdl.Sdl.Scancode.v;
+    Tsdl.Sdl.Scancode.t;    Tsdl.Sdl.Scancode.y;
+    Tsdl.Sdl.Scancode.g;    Tsdl.Sdl.Scancode.h;
+    Tsdl.Sdl.Scancode.b;    Tsdl.Sdl.Scancode.n;
+    Tsdl.Sdl.Scancode.u;    Tsdl.Sdl.Scancode.i;
+    Tsdl.Sdl.Scancode.j;    Tsdl.Sdl.Scancode.k;
+    Tsdl.Sdl.Scancode.m;
+    Tsdl.Sdl.Scancode.o;    Tsdl.Sdl.Scancode.p;
+    Tsdl.Sdl.Scancode.l;
   ]
 
 let make (g : Grammar.t) : (t, string) result =
@@ -31,7 +38,7 @@ let key_name (k : key) : string =
   Tsdl.Sdl.get_scancode_name k
 
 let print (m : t) : unit =
-  print_endline "SDL2 Key mappings (scancodes):";
+  print_endline "Key mappings :";
   List.iter
     (fun (k, t) -> Printf.printf "  %s -> %s (%s)\n"
       (key_name k) t.Grammar.label t.token)
